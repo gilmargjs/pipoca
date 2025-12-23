769feeebedb8e93fc1ae858e1981793392ef7be8
gilmar <gilmarjose2014@gmail.com>
2025-12-22 21:52:47 -0300

    oringin

--- pipoca/forms.py
+++ pipoca/forms.py
@@ -5,4 +5,10 @@ class PipocaModelForm(forms.ModelForm):
     class Meta:
         model = Pipoca
         fields = '__all__'    
-
+    #validação de preço do produto o valor deve ser acima de 2,00
+    def clean_preco(self):
+        preco = self.cleaned_data.get('preco')
+        if preco < 2:
+            self.add_error('preco', 'Valor para cadastro deve ser acima de R$2,00')
+        else:
+            return preco
