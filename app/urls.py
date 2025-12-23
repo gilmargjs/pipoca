801afe71ac5b1ae79fe0faa7e175be76b4f502a0
gilmar <gilmarjose2014@gmail.com>
2025-12-22 23:01:40 -0300

    criacao de app accounts

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from pipoca.views import pipoca_view, new_pipoca_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register'),
     path('pipoca/', pipoca_view, name='pipoca_list'),
     path('new_pipoca/', new_pipoca_view, name='new_pipoca'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
